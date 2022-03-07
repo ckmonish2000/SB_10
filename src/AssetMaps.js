080b2b9e071665a822e25258cf02b4ea45c3cd93
monish karunakaran <ckmonish2000@gmail.com>
2022-03-07 10:01:13 +0530

    altered useallasses and new assetmap added

---
+++ src/AssetMaps.js
@@ -0,0 +1,36 @@
+import { imgUrl, soundUrl, lottieUrl } from "../utils/path"
+
+const AllAssetMap = {
+  // backgrounds & common lottie
+  Bg: {
+    id: "Backgrounds",
+
+    Bg: ``,
+
+    sprites: [
+      `${imgUrl}sb_43/Intro_bg.svg`,
+      `${imgUrl}sb_43/Intro-bg_Text.svg`,
+      `${imgUrl}sb_43/Road_bg.svg`,
+      `${imgUrl}sb_43/Tree_bg.svg`,
+      `${imgUrl}sb_43/Wall_bg.svg`,
+      `${imgUrl}sb_43/Garage.svg`,
+      `${imgUrl}sb_43/Well_Done_bg.svg`,
+    ],
+
+
+    sounds: [
+      // `${soundUrl}Intro.mp3`
+    ],
+
+    lottie: [
+      `${lottieUrl}intro/bluecharacter.json`,
+      `${lottieUrl}intro/yellowcharacter.json`,
+      `${lottieUrl}scene3/Ch_Walking.json`,
+      `${lottieUrl}scene3/paint_green.json`,
+      `${lottieUrl}scene4/Blue_Ch_Walk.json`,
+      `${lottieUrl}scene4/wipe_paint.json`,
+
+    ]
+  },
+
+}
