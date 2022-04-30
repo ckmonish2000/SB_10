4a3b6bd73f824bb1f5b7d99bb2e832999d8c314f
monish karunakaran <ckmonish2000@gmail.com>
2022-04-30 20:41:27 +0530

    working mute and bg music

--- src/Scenes/Home.js
+++ src/Scenes/Home.js
@@ -25,6 +25,7 @@ export default function Home({ play }) {
   useEffect(() => {
     if (!Loading && !Scene1.Loading) {
       setSceneId("/Scene1")
+      play()
     }
   }, [Loading, Scene1.Loading])
 
