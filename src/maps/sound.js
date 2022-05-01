39565fc0be30da745137568a14ec0d1dc30376e9
monish karunakaran <ckmonish2000@gmail.com>
2022-05-01 14:16:31 +0530

    added sounds map

---
+++ src/maps/sound.js
@@ -0,0 +1,47 @@
+import { imgUrl, soundUrl, lottieUrl } from "../utils/path"
+const url = `${soundUrl}/fruit`
+const url2 = `${soundUrl}/veg`
+
+const SoundMap = {
+  fruits: [
+    `${url}Apple.mp3`,
+    `${url}Banana.mp3`,
+    `${url}Cherry.mp3`,
+    `${url}Green-Grapes.mp3`,
+    `${url}Litchi.mp3`,
+    `${url}Mango.mp3`,
+    `${url}Muskmelon.mp3`,
+    `${url}Orange.mp3`,
+    `${url}Papaya.mp3`,
+    `${url}Peach.mp3`,
+    `${url}Pear.mp3`,
+    `${url}Pineapple.mp3`,
+    `${url}Plum.mp3`,
+    `${url}Strawberry.mp3`,
+    `${url}Watermelon.mp3`,
+    `${url}Sweetlime.mp3`,
+  ],
+  veg: [
+    `${url2}Brinjal.mp3`,
+    `${url2}Cabbage.mp3`,
+    `${url2}Capsicum.mp3`,
+    `${url2}Carrot.mp3`,
+    `${url2}Colliflower.mp3`,
+    `${url2}Corn.mp3`,
+    `${url2}Cucumber.mp3`,
+    `${url2}Ladyfinger.mp3`,
+    `${url2}Lattuce.mp3`,
+    `${url2}Onion.mp3`,
+    `${url2}Peas.mp3`,
+    `${url2}Potato.mp3`,
+    `${url2}Pumpkin.mp3`,
+    `${url2}Raddish.mp3`,
+    `${url2}Red-Pepper-Pieces.mp3`,
+    `${url2}Spinach.mp3`,
+    `${url2}Tomato.mp3`,
+    `${url2}Yellow-Pepper-Pieces.mp3`,
+  ],
+
+}
+
+export default SoundMap
